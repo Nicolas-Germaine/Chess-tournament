fa9145e9f2248a5c1c20376f31717481105b2c12
Nicolas-Germaine <85984707+Nicolas-Germaine@users.noreply.github.com>
2021-09-03 13:52:43 +0200

    Update tinyDB.py
    voir le #

--- tinyDB.py
+++ tinyDB.py
@@ -34,3 +34,40 @@ class PlayerView:
             nb += 1
 
         return list_player"""
+
+# Création d'une classe pour tinyDB, je n'arrive pas à écrire ds le fichier.json même si le code semble ok
+"""
+class Player:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+db = TinyDB('db.json')
+player_table = db.table('player')
+
+player1 = Player(first_name=input("Tapez le prénom >> "), last_name=input("Tapez le nom de famille >> "))
+player2 = Player(first_name='Axel', last_name='Germaine')
+
+serialized_player1 = {
+    'first_name': player1.first_name,
+    'last_name': player1.last_name
+}
+
+serialized_player2 = {
+    'first_name': player2.first_name,
+    'last_name': player2.last_name
+}
+
+first_name = serialized_player1['first_name']
+last_name = serialized_player1['last_name']
+player_1 = Player(first_name=first_name, last_name=last_name)
+
+first_name = serialized_player2['first_name']
+last_name = serialized_player2['last_name']
+player_2 = Player(first_name=first_name, last_name=last_name)
+
+
+print(serialized_player1)
+print(serialized_player2)
+"""
