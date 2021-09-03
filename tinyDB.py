34a95413e50169978567f7c8619e6698241b0932
Nicolas Germaine <cariadfreno@gmail.com>
2021-09-03 13:50:14 +0200

    tinyDB prob d'écriture ds fichier .json

--- tinyDB.py
+++ tinyDB.py
@@ -2,13 +2,16 @@ from tinydb import TinyDB, Query
 from pprint import pprint
 
 db = TinyDB('db.json')
-fruits = db.table('8 players')
+list_player = db.table('8 players')
+nb = 0
 
-fruits.insert({'first_name': input("Tapez le prénom >>")})
-fruits.insert({'last_name': input("Tapez le nom de famille >> ")})
-fruits.insert({'date_of_birth': input("Tapez la date de naissance (ex 12/02/1987) >> ")})
-fruits.insert({'gender': input("Sexe ? (H/F) >> ")})
-fruits.insert({'ranking': input("Son rang ? >> ")})
+while nb < 2:
+    list_player.insert({'first_name': input("Tapez le prénom >> ")})
+    list_player.insert({'last_name': input("Tapez le nom de famille >> ")})
+    list_player.insert({'date_of_birth': input("Tapez la date de naissance (ex 12/02/1987) >> ")})
+    list_player.insert({'gender': input("Sexe ? (H/F) >> ")})
+    list_player.insert({'ranking': input("Son rang ? >> ")})
+    nb += 1
 
 
-pprint(fruits.all())
+pprint(list_player.all())
