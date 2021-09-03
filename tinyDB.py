6429be8667849d2b960feaf501cd1ae54304e13c
Nicolas Germaine <cariadfreno@gmail.com>
2021-09-03 11:26:42 +0200

    commit du 03/09/2021, avancement tinyDB

--- tinyDB.py
+++ tinyDB.py
@@ -1,36 +1,14 @@
-from tinydb import TinyDB, Query, where
+from tinydb import TinyDB, Query
+from pprint import pprint
 
 db = TinyDB('db.json')
+fruits = db.table('8 players')
 
-class NewPlayers:
-    def __init__(self, first_name, last_name, date_of_birth, gender, ranking):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.date_of_birth = date_of_birth
-        self.gender = gender
-        self.ranking = ranking
-
-class PlayerView:
-    """Player game view."""
-
-    def information_player(self):
-        """User view information for player creation."""
-        list_player = []
-        nb = 0
-
-        while nb < 4:
-
-            first_name = input("Tapez le prénom >> ")
-            last_name = input("Tapez le nom de famille >> ")
-            date_of_birth = input("Tapez la date de naissance (ex 12/02/1987) >> ")
-            gender = input("Sexe ? (H/F) >> ")
-            ranking = input("Son rang ? >> ")
-            players = NewPlayers(first_name, last_name, date_of_birth, gender, ranking)
-            list_player.append(players)
-            nb += 1
-
-        return list_player
+fruits.insert({'first_name': input("Tapez le prénom >>")})
+fruits.insert({'last_name': input("Tapez le nom de famille >> ")})
+fruits.insert({'date_of_birth': input("Tapez la date de naissance (ex 12/02/1987) >> ")})
+fruits.insert({'gender': input("Sexe ? (H/F) >> ")})
+fruits.insert({'ranking': input("Son rang ? >> ")})
 
 
-print(PlayerView.information_player(self=None))
-
+pprint(fruits.all())
