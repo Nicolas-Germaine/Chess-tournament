f87e63f723cd904480e0153eaac26a1b3869b210
Nicolas-Germaine <85984707+Nicolas-Germaine@users.noreply.github.com>
2021-10-08 19:36:21 +0200

    Maj

--- models/player.py
+++ models/player.py
@@ -7,8 +7,8 @@ class NewPlayers:
         self.last_name = last_name
         self.date_of_birth = date_of_birth
         self.gender = gender
-        self._ranking = ranking
-        self._point = 0
+        self.ranking = ranking
+        self.point = 0
         """liste contenant les playeurs qui se rencontre, si ils ne se sont pas rencontrer ils peuvent faire le match"""
         self.fight = set()
 
@@ -20,7 +20,7 @@ class NewPlayers:
         if type(autre) is not type(self):
             raise TypeError
 
-        return (-self._point, self._ranking) < (-autre._point, autre._ranking)
+        return (-self.point, self.ranking) < (-autre.point, autre.ranking)
 
 
     def __str__(self):
