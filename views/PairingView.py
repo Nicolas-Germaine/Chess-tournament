c87f1a6194240d1720a2b64d354bf0247aa9f944
Nicolas-Germaine <85984707+Nicolas-Germaine@users.noreply.github.com>
2021-10-08 19:33:40 +0200

    Maj

--- views/PairingView.py
+++ views/PairingView.py
@@ -10,18 +10,18 @@ class Infos:
             print("======================")
             numb = input(f"Entre 1 si le joueur {player_1} est vainquer ou 0 si c'est un match nul ou 2 pour le joueur {player_2}  ")
             if int(numb)==1:
-                player_1._point +=1
+                player_1.point +=1
                 player_1.fight.add(player_2)
                 # player_2.fight.add(player_1)
                 print("Le vainqueur est ",player_1)
             elif int(numb)==2:
-                player_2._point +=1
+                player_2.point +=1
                 player_2.fight.add(player_1)
                 # player_1.fight.add(player_2)
                 print("Le vainqueur est ",player_2)
             elif int(numb)==0:
-                player_1._point +=0.5
-                player_2._point +=0.5
+                player_1.point +=0.5
+                player_2.point +=0.5
                 player_1.fight.add(player_2)
                 print("Match Nul")
             list_joueurs.append(player_1)
