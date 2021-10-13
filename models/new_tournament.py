4eba11f12583548c3dab21c1262ef4a8dbde644d
Nicolas Germaine <cariadfreno@gmail.com>
2021-10-13 16:54:29 +0200

    maj

--- models/new_tournament.py
+++ models/new_tournament.py
@@ -6,8 +6,13 @@ TIME = ("Blitz", "Bullet", "Speed")
 class Tournament:
     """Information for a new tournament"""
 
-    def __init__(self, name, place, date):
+    def __init__(self, name, place, date, number_of_turns=4, tours, players, time, comments):
         """Init information of tournament"""
         self.name = name
         self.place = place
         self.date = date
+        self.number_of_turns = number_of_turns
+        self.tours = tours
+        self.players = players
+        self.time = time
+        self.comments = comments
