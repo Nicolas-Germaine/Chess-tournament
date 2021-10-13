c12aab1ce2da1c00371e83fbb82ad5ff6547c957
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-10-13 14:43:26 +0000

    get

--- models/old_tournament.py
+++ models/old_tournament.py
@@ -8,8 +8,9 @@ class Tournament:
 
     def list_of_tournament(self):
 
-        liste = [(tournament_table.search(all))]
-        print(liste)
+        # liste = [(tournament_table.search(all))]
+        contents = [item['name'] for item in db.table('tournament_table').all()]
+        print(contents)
         input = (">>> ")
         choice = []
 
