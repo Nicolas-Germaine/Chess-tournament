4b31f7d5a16606ded7b28db4dc726380f8c2bdaa
Nicolas Germaine <cariadfreno@gmail.com>
2021-10-22 08:44:47 +0200

    get

--- models/old_tournament.py
+++ models/old_tournament.py
@@ -1,6 +1,6 @@
 #from ..controllers.controllers import MakePairPlayer
-from tinydb import TinyDB
-
+from ..models.player import NewPlayers
+from tinydb import TinyDB, where
 db = TinyDB("./Chess-tournament/db.json")
 tournament_table = db.table('tournament_table')
 player_table = db.table('player_table')
@@ -9,20 +9,31 @@ class Tournament:
 
     def list_of_tournament(self):
 
-        tournament = []
         contents = [item['name'] for item in db.table('tournament_table').all()]
         print(f"Liste des tournois dispo >>> {contents}")
-        choice = input("Entrez 1 pour le premier tournoi, 2 pour le second... >>> ")
-        tournament.append(choice)
+        choice = str(input("Rentrez le nom du tournoi >>> "))
+        table_tournament = db.table('tournament_table')
+        name = tournament_table.search(where('name') == choice)
+        #print("============", type(list(name[0]['players'].split(" "))))
+        ##########################################################################
+        for i in list(name[0]['players'].split(" ")):
+            req_players = player_table.search(where('prenom') == str(i))
+            print("===========", i)
+            try:
+                new_player = NewPlayers(req_players[0]['prenom'],
+                                        req_players[0]['nom de famille'],
+                                        req_players[0]['date de naissance'],
+                                        req_players[0]['sexe'],
+                                        req_players[0]['rang'])
+                print(new_player)
+            except:
+                pass
 
-        if tournament == ['1']:
-            contents = [item['prenom'] for item in db.table('player_table').all()]
-            print(f'Les players sont >>> {contents}')
-        else:
-            print('Oops...')
+        return name[0]['players']
 
 
 
-
+# Crud = Create / Read / Update / Delete
+# ex -> python tinyDB create
 
 
--- models/player.py
+++ models/player.py
@@ -2,7 +2,7 @@
 class NewPlayers:
     """add players for tournament"""
 
-    def __init__(self, first_name, last_name, date_of_birth, gender, tournament, ranking=0):
+    def __init__(self, first_name, last_name, date_of_birth, gender, tournament, ranking):
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
