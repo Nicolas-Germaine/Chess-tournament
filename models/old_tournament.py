b291cc08ce404ea55568e1dcbe81044f24477786
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-10-23 18:05:11 +0000

    get

--- models/old_tournament.py
+++ models/old_tournament.py
@@ -1,5 +1,5 @@
 #from ..controllers.controllers import MakePairPlayer
-from tinydb import TinyDB
+from tinydb import TinyDB,Query,where
 
 db = TinyDB("./Chess-tournament/db.json")
 tournament_table = db.table('tournament_table')
@@ -9,17 +9,22 @@ class Tournament:
 
     def list_of_tournament(self):
 
-        tournament = []
         contents = [item['name'] for item in db.table('tournament_table').all()]
         print(f"Liste des tournois dispo >>> {contents}")
-        choice = input("Entrez 1 pour le premier tournoi, 2 pour le second... >>> ")
-        tournament.append(choice)
+        choice = input("Entrez le Nom du tournoi... >>> ")
+        # tournament.append(choice)
 
-        if tournament == ['1']:
-            contents = [item['prenom'] for item in db.table('player_table').all()]
-            print(f'Les players sont >>> {contents}')
-        else:
-            print('Oops...')
+        # if tournament == ['1']:
+        #     contents = [item['prenom'] for item in db.table('player_table').all()]
+        #     print(f'Les players sont >>> {contents}')
+        # else:
+        #     print('Oops...')
+        table_tournament = db.table('tournament_table')
+        tournament = table_tournament.search(Query().name == choice)
+        # print(tournament[0]['players'])
+        print(f"les joueurs sont {tournament[0]['players']} ")
+        for item in tournament[0]['players']:
+            print(item.first_name)
 
 
 
